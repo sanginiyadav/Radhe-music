d46630f03ba007609cc54b016096456df7d49b26
radhe-music1 <ubantu25u7@gmail.com>
2025-10-22 20:21:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,11 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://sachinapi.thequickearn.xyz')
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", "NxGBNexGenBots1ecb2c")
+
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/radhe-music1/shizuka-nand",
